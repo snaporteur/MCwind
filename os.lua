59f8dc0b488e13f1a3a5b0a88f47cac4b92bd664
snaporteur <72853501+snaporteur@users.noreply.github.com>
2021-10-29 19:18:48 +0200

    Update os.lua

--- os.lua
+++ os.lua
@@ -4,7 +4,7 @@ local term = require("term")
 
 term.clear()
 gpu.setBackground(0x023f63)
-gpu.fill(0, 0, 10, 10, " ")
+gpu.fill(0, 0, 200, 200, " ")
 gpu.set(2, 5, "Bienvenue sur canardOS")
 
 while true do
