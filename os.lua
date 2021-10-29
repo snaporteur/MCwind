91a7bfc98e4e9b49ead4b7dc3e62fbfc62cea8d7
snaporteur <72853501+snaporteur@users.noreply.github.com>
2021-10-29 19:06:57 +0200

    Update os.lua

--- os.lua
+++ os.lua
@@ -2,14 +2,19 @@ local component = require("component")
 local gpu = component.gpu
 local term = require("term")
 
+started = true
+
 local function background_start()
   gpu.setBackground(0x023f63)
-  gpu.fill(0, 0, 10000, 10000, " ")
+  gpu.fill(0, 0, 10, 10, " ")
 end
 
 term.clear()
-background_start()
 
 while true do
+  if started do
+    background_start()
+    term.clear()
+  end
   os.sleep(1)
 end
