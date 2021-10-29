e98bca69f130310614c3b1fb611c4d2a332c4648
snaporteur <72853501+snaporteur@users.noreply.github.com>
2021-10-29 18:55:24 +0200

    Update os.lua

--- os.lua
+++ os.lua
@@ -3,7 +3,8 @@ local gpu = component.gpu
 local term = require("term")
 
 local function background()
-  gpu.set(2, 2, "test")
+  gpu.setBackground(0xffffff)
+  gpu.fill(0, 0, 10, 10, " ")
 end
 
 while true do
