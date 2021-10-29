190b27a4c6e56d74b8fac1985b1b7b21857af17a
snaporteur <72853501+snaporteur@users.noreply.github.com>
2021-10-29 18:44:13 +0200

    Update os.lua

--- os.lua
+++ os.lua
@@ -2,6 +2,11 @@ local component = require("component")
 local gpu = component.gpu
 local term = require("term")
 
+local function background()
+  gpu.set(2, 2, "test")
+end
+
 while true do
   term.clear()
+  background()
 end
